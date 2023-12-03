9fac10dae8d5329cdac85750b6c8b88201ed8a46
EdwardBO97 <kevinedwardbo@gmail.com>
2023-12-03 18:40:05 -0400

    Cambio de Template, CRUD de Categorías

--- Sis457ComputadorasG3/WebComputadorasG3/obj/Debug/net7.0/WebComputadorasG3.AssemblyInfo.cs
+++ Sis457ComputadorasG3/WebComputadorasG3/obj/Debug/net7.0/WebComputadorasG3.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebComputadorasG3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77661318591e59ea172c0b1c7498f0d620b5d18d")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebComputadorasG3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebComputadorasG3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
